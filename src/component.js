e1256a5c156a003b88d855c31e80f4bc8fb5c903
Aruna1990 <1017124510@qq.com>
2017-02-14 15:52:21 +0800

    add class User

--- src/component.js
+++ src/component.js
@@ -7,3 +7,4 @@ export default function component () {
 
 	return element;
 }
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import component from './component'
+import component from './component';
+import User from './User';
 
 document.body.appendChild(component());
+
+let user = new User('David', 'Chen');
+console.log(user.fullName());
